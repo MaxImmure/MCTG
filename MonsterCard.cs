dffe220f42ee92f5b16767ba662e1a8456362a5a
SURFACE-PRO-6\if22b190 <if22b190@technikum-wien.at>
2022-09-20 21:23:52 +0200

    first commit

---
+++ MonsterCard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTCG
+{
+    public abstract class MonsterCard : ICard
+    {
+    }
+
+    public abstract class SpellCard : ICard
+    {
+    }
+}
