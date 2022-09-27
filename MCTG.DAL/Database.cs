49c99f78de3dde54155765104da7db093d6cd4aa
MaxImmure <if22b190@technikum-wien.at>
2022-09-27 17:53:55 +0200

    implemented SubProjects

---
+++ MCTG.DAL/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.BL
+{
+    class Database
+    {
+        private static Database _instance = null;
+
+        private Database()
+        {
+
+        }
+
+        public static Database getInstance()
+        {
+            if (_instance == null)
+                _instance = new Database();
+            return _instance;
+        }
+
+        public void query(string query)
+        {
+            //ToDo
+        }
+    }
+}
