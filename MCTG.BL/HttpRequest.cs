0876d75e822901771324426023dc9445989c1351
Maximilian Mörth <maxi.moerth@outlook.com>
2022-11-08 22:52:30 +0100

    http handler from class lession 8.11

---
+++ MCTG.BL/HttpRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTG.BL
+{
+    internal class HttpRequest
+    {
+        private TcpClient socket;
+        public string Url { get; }
+        public string HttpMethod { get; }
+        public string[] UrlSegments { get { return Url.Split("/").Skip(1).ToArray(); } }
+
+        public HttpRequest(TcpClient socket)
+        {
+            this.socket = socket;
+            using var reader = new StreamReader(socket.GetStream());
+            //Fehlerbehandlung
+            var httpParts = reader.ReadLine().Split(" ");
+            HttpMethod = httpParts[0];
+            Url = httpParts[1];
+            //Header & Request Content ?
+        }
+    }
+}
---
+++ MCTG.BL/HttpServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MCTG.BL
+{
+    class Server
+    {
+        public IPAddress host { get; }
+        public int port { get; }
+        private TcpListener listener;
+
+        //DAtabase
+
+        public Server(int port = 10001)
+        {
+            this.port = port;
+            host = IPAddress.Any;
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public async void Run()
+        {
+            //db.connect();
+            listener.Start();
+
+            for (; ; )
+            {
+                try
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+
+
+                    Console.Write("Handling client at " + client.Client.RemoteEndPoint + " - ");
+
+                    Task.Run(() => {
+                        //ToDo Magic
+                    });
+
+                    //client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+    }
+
+}
