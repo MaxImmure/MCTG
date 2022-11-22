5b7f584ed34a8582cce4ddf7a1e8ebfab141d069
Maximilian Mörth <maxi.moerth@outlook.com>
2022-11-22 20:01:08 +0100

    updated database class

--- MCTG.DAL/Database.cs
+++ MCTG.DAL/Database.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using Npgsql;
+using Npgsql.Replication.PgOutput.Messages;
+using NpgsqlTypes;
 
 namespace MCTG.BL
 {
     class Database
     {
         private static Database _instance = null;
+        private IDbConnection connection;
 
         private Database()
         {
-
+            //ToDo extract connection String    
+            connection = new NpgsqlConnection($"Host=localhost;Username={"swe1user"};Password={"swe1pw"};Database={"mctg_game"}");
         }
 
         public static Database getInstance()
