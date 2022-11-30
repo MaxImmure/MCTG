f8e32ce9936d8bb4ad8e97dc1ee56a218ad22afc
Maximilian Mörth <maxi.moerth@outlook.com>
2022-11-30 04:04:24 +0100

    updated db & server

---
+++ MCTG.Models/HttpRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTG.Models
+{
+    public class HttpRequest
+    {
+        private TcpClient socket;
+        public string Url { get; private set; }
+        public string HttpMethod { get; private set; }
+        public string ContentType { get; private set; }
+
+        public HttpRequest(TcpClient socket)
+        {
+            this.socket = socket;
+        }
+
+    }
+}
---
+++ MCTG.Models/HttpResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCTG.Models
+{
+    public class HttpResponse
+    {
+        private TcpClient socket;
+        public string StatusCode { get; private set; }
+        public string HttpVersion { get; private set; }
+
+
+        public HttpResponse(TcpClient socket)
+        {
+            this.socket = socket;
+        }
+
+    }
+}
