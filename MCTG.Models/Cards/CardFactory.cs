c70876b9604e2d59a134fb07c2da67e4a5320793
Maximilian Mörth <maxi.moerth@outlook.com>
2023-01-06 08:37:45 +0100

    implemented FactoryPattern for Cards ToDo Unittests

--- MCTG.Models/Cards/CardFactory.cs
+++ MCTG.Models/Cards/CardFactory.cs
@@ -3,11 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MCTG.Models.Cards.MonsterCards;
+using MCTG.Models.Cards.SpellCards;
 
 namespace MCTG.Models.Cards
 {
+    public enum CardType
+    {
+        Spell,
+        Monster,
+        Empty
+    }
+
+    public enum ElementType
+    {
+        Regular,
+        Water,
+        Fire,
+        NaE
+    }
+
     public class CardFactory
     {
-        //ToDo
+        private static readonly Dictionary<string, Func<double, ICard>> Cards = new()
+        {
+            ["Empty"] = dmg => EmptyCard.Instance(),
+            ["FireSpell"] = dmg => new FireSpellCard(dmg),
+            ["WaterSpell"] = dmg => new WaterSpellCard(dmg),
+            ["FireElf"] = dmg => new FireElfCard(dmg),
+            ["WaterElf"] = dmg => new WaterElfCard(dmg),
+            ["RegularElf"] = dmg => new RegularElfCard(dmg),
+            ["FireGoblin"] = dmg => new FireGoblinCard(dmg),
+            ["WaterGoblin"] = dmg => new WaterGoblinCard(dmg),
+            ["RegularGoblin"] = dmg => new RegularGoblinCard(dmg),
+            ["Knight"] = dmg => new KnightCard(dmg),
+            ["Kraken"] = dmg => new KrakenCard(dmg),
+            ["Ork"] = dmg => new OrkCard(dmg),
+            ["FireTroll"] = dmg => new FireTrollCard(dmg),
+            ["WaterTroll"] = dmg => new WaterTrollCard(dmg),
+            ["RegularTroll"] = dmg => new RegularTrollCard(dmg),
+            ["FireWizzard"] = dmg => new FireWizzardCard(dmg),
+            ["WaterWizzard"] = dmg => new WaterWizzardCard(dmg),
+            ["RegularWizzard"] = dmg => new RegularWizzardCard(dmg),
+            ["Dragon"] = dmg => new DragonCard(dmg)
+        };
+
+        public static ICard GetCard(CardType type, ElementType element, string cardName, double damage)
+        {
+            switch (type)
+            {
+                case (CardType.Empty):
+                    return Cards[type.ToString()].Invoke(damage); //ToDo Test
+                case (CardType.Spell):
+                    return Cards[element.ToString() + type.ToString()].Invoke(damage);
+                case CardType.Monster:
+                    switch (element)
+                    {
+                        case ElementType.NaE:
+                            return Cards[cardName].Invoke(damage);
+                        default: return Cards[element.ToString() + cardName].Invoke(damage);
+                    }
+            }
+            //ToDo
+
+            return EmptyCard.Instance();
+        }
     }
 }
