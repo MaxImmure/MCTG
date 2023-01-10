d24a320498c6f34e3d0869aae6b930565baed4e8
Maximilian Mörth <maxi.moerth@outlook.com>
2023-01-10 17:41:51 +0100

    updated game logic (added log)

--- MCTG.BL/Battle.cs
+++ MCTG.BL/Battle.cs
@@ -10,22 +10,63 @@ namespace MCTG.BL
     public class Battle
     {
         User player1, player2;
+        Random rnd;
 
         public Battle(User player1, User player2)
         {
             this.player1 = player1;
             this.player2 = player2;
+            rnd = new Random(); //generate new Random each lobby
         } 
 
         public string startBattle(User p1, User p2)
         {
+            var log = new StringBuilder();
             List<ICard> deckP1 = p1.Deck.ToList()
             , deckP2 = p2.Deck.ToList();
-            //ToDo Logging
+            var rnd = new Random();
+            //ToDo Logging and return log
+            ICard currentP1, currentP2;
+
             for (int round = 1; round <= 100; round++)
             {
+                //Choose random Card for each player
+                //"<Card Picking Phase>"
+                var pickCards = chooseRandomCards(deckP1, deckP2);
+                currentP1 = pickCards.Item1;
+                log.AppendLine($"Player1 chooses {currentP1.ToString()}"); //ToDo ToString
+                currentP2 = pickCards.Item2;
+                log.AppendLine($"Player2 chooses {currentP2.ToString()}"); //ToDo ToString
+
+                log.AppendLine($"Fighting Phase\n\t{currentP1.ToString()} vs. {currentP2.ToString()}");
                 
+                var winnerCard = simulateCardFight(currentP1, currentP2);
+                if (winnerCard == EmptyCard.Instance())
+                {
+                    log.AppendLine("DRAW:\tNo Winner this Round!");
+                    continue;
+                }
+
+                log.AppendLine($"WINNER:\t{winnerCard.ToString()}");
+
+                //ToDo Move looserCard to PlayerWinner
+                //End Condition if its not endless draw, deck.Count == 0
             }
+            return log.ToString();//live ping before lobby starts?
+        }
+
+        private Tuple<ICard, ICard> chooseRandomCards(IList<ICard> deckP1, IList<ICard> deckP2)
+        {
+            try
+            {
+                return new(
+                        deckP1.ElementAt(rnd.Next(0, deckP1.Count))
+                        , deckP2.ElementAt(rnd.Next(0, deckP2.Count)));
+            } catch (Exception)
+            {
+                throw new NotImplementedException();//ToDo
+            }
+            //ToDo unable to Pick
             return null;
         }
 
@@ -40,11 +81,5 @@ namespace MCTG.BL
             return EmptyCard.Instance();
         }
 
-
-        private Tuple<ICard,ICard> chooseRandomCards(ICard[] deck1, ICard deck2)
-        {
-            //ToDo
-            return null;
-        }
     }
 }
--- MCTG.Models/Cards/EmptyCard.cs
+++ MCTG.Models/Cards/EmptyCard.cs
@@ -9,26 +9,15 @@ namespace MCTG.Models.Cards
     public class EmptyCard : ICard
     {
         private static EmptyCard _instance;
-        private EmptyCard()
-        {
-            this.damage = -1.0;
-        }
-
-        public static EmptyCard Instance()
-        {
-            return _instance ??= new();
-        }
 
         public readonly double damage;
 
-        public double DamageModifier(ICard opponentCard)
-        {
-            return -1;
-        }
+        private EmptyCard() => this.damage = -1.0;
 
-        public double getDamage(ICard opponentCard)
-        {
-            return -1;
-        }
+        public static EmptyCard Instance() => _instance ??= new();
+
+        public double DamageModifier(ICard opponentCard) => -1;
+
+        public double getDamage(ICard opponentCard) => -1;
     }
 }
